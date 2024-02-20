eb3b4ca96f6328be4135fa10f8d12fbff8e511e5
donghyukkil <asterism90@gmail.com>
2024-02-20 09:26:50 +0900

    refactor: DropDownButton 커스텀 위젯으로 변경

--- lib/components/repository_dropdown_button.dart
+++ lib/components/repository_dropdown_button.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class RepositoryDropdownButton extends StatelessWidget {
+class RepositoryDropdownButton extends StatefulWidget {
   final String? selectedRepository;
   final List<String> repositories;
   final ValueChanged<String?> onChanged;
@@ -13,17 +13,63 @@ class RepositoryDropdownButton extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  _RepositoryDropdownButtonState createState() =>
+      _RepositoryDropdownButtonState();
+}
+
+class _RepositoryDropdownButtonState extends State<RepositoryDropdownButton> {
+  late String? currentItem;
+
+  @override
+  void initState() {
+    super.initState();
+
+    currentItem = widget.selectedRepository ??
+        (widget.repositories.isNotEmpty ? widget.repositories.first : null);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      isExpanded: true,
-      value: selectedRepository,
-      onChanged: onChanged,
-      items: repositories.map<DropdownMenuItem<String>>((String value) {
-        return DropdownMenuItem<String>(
-          value: value,
-          child: Text(value),
-        );
-      }).toList(),
+    return PopupMenuButton<String>(
+      onSelected: (String value) {
+        setState(() {
+          currentItem = value;
+        });
+
+        widget.onChanged(value);
+      },
+      itemBuilder: (BuildContext context) {
+        return widget.repositories.map((String choice) {
+          return PopupMenuItem<String>(
+            value: choice,
+            child: Text(
+              choice,
+              overflow: TextOverflow.ellipsis,
+            ),
+          );
+        }).toList();
+      },
+      offset: const Offset(0, 40),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: IntrinsicWidth(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Flexible(
+                child: Text(
+                  currentItem ?? 'Select repository',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.w500),
+                ),
+              ),
+              const SizedBox(width: 50),
+              const Icon(Icons.arrow_drop_down),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
